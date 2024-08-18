cc8afc992fab1e90fd7ad2a80c85f5bb7b264496
dynamo58 <sailorfirex@seznam.cz>
2024-08-18 14:45:34 +0200

    init

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+const $ = (s) => document.getElementById(s);
+
+const NotifSeverity = {
+  // values must match up with those defined in /styles/main.css
+  Success: "success",
+  Warning: "warning",
+  Error: "error",
+};
+
+async function spawnNotification(severity, message) {
+  const notificationContainer = $("notification-container");
+
+  const notification = document.createElement("div");
+  notification.classList.add("notification", severity);
+  notification.innerText = message;
+
+  notificationContainer.appendChild(notification);
+
+  setTimeout(() => {
+    notificationContainer.removeChild(notification);
+  }, 3000);
+}
+
+async function copyToClipboard(str, customNotiMsg) {
+  await navigator.clipboard.writeText(str);
+
+  const msg = customNotiMsg ? customNotiMsg : "text copied to clipboard";
+  spawnNotification(NotifSeverity.Success, msg);
+}
